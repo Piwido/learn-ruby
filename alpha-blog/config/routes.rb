4f0cfaff49c6eec5030f3774bbd3ce1d6e2d8d0c
Ines Boukhars <ines.boukhars@enseirb.fr>
2024-07-04 10:29:31 +0200

    add articles listing features

--- alpha-blog/config/routes.rb
+++ alpha-blog/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show] #Creates the route needed
+  resources :articles, only: [:show, :index] #Creates the route needed
 
 
 
