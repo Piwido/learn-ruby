aa6ec38b4213c3f713d66921018a59febc23fff1
Ines Boukhars <ines.boukhars@enseirb.fr>
2024-07-05 15:26:42 +0200

    back: ajout des validations entrées utilisateurs

--- alpha-blog/app/models/user.rb
+++ alpha-blog/app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+    validates :username, uniqueness: {case_sensitive: false},   
+                        presence: true, length: { minimum: 3, maximum: 25}
+    VALID_EMAIL_REGEX =  /\A[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]+\z/i #i for case insensitive
 
+    validates :email, presence: true, length: { maximum: 105 }, 
+                        uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX, message: "invalid email format"}
 end
